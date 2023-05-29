4c15255cfc43591762903e3cbb0426a2e9c367e6
Eshgin Farzaliyev <eshqinferzeliyev@gmail.com>
2023-05-29 11:24:54 +0400

    feat: asnyc await

--- promise/app.js
+++ promise/app.js
@@ -1,6 +1,5 @@
 const url = `https://jsonplaceholder.typicode.com/users`
 
-
 // fetch(url)
 // .then((resp)=>resp.json()
 // .then((data)=> {
@@ -11,12 +10,30 @@ const url = `https://jsonplaceholder.typicode.com/users`
 // })
 // )
 
-fetch(url)
-.then((resp)=>resp.json()
-.then((data)=> {
-    data.forEach((user) => {
-        document.write(`<p>${user.id} ${user.name}</p>`) 
-    });
+// fetch(url)
+// .then((resp)=>resp.json()
+// .then((data)=> {
+//     data.forEach((user) => {
+//         document.write(`<p>${user.id} ${user.name}</p>`) 
+//     });
   
-})
-)
+// })
+// )
+
+
+//async await
+
+const BASE_URL = `https://jsonplaceholder.typicode.com`
+
+async function getData(_url){
+    const {data} = await axios.get(_url)
+    return data
+}
+
+async function writeUsers(){
+    const users = await getData(BASE_URL+`/users`)
+    users.forEach((user)=>{
+        document.write(`<p>${user.name}</p>`)
+    })
+}
+writeUsers()
